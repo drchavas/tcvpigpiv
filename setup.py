d44e05055e65b36639fa0201fd6fe6addeb6ce4a
dchavas <drchavas@gmail.com>
2025-05-16 14:08:18 -0400

    removing ipynb test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tcwindprofile',
-    version='0.1.22',
+    version='0.1.23',
     packages=find_packages(),
     install_requires=[],
     author='Dan Chavas',
